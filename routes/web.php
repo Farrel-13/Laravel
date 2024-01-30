db03537bf9ffd554ca93fd6b4fbe11576c0de16d
Farrel <muhammadfarrelargiyanto@gmail.com>
2024-01-30 08:20:19 +0700

    add new route

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\UserController;
 
 Route::redirect('/', '/users');
 
+Route::redirect('/', '/admin');
+
 Route::prefix('users')->group(function() {
     Route::get('/', [UserController::class, 'index']);
 });
